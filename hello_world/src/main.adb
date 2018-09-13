267667975a6f03bf0c6a4a8bf3cec7e83efc7383
Tama McGlinn <TGJ.McGlinn@mindef.nl>
2018-09-13 10:06:54 +0200

    Added use of discriminants to hello world

--- hello_world/src/main.adb
+++ hello_world/src/main.adb
@@ -1,7 +1,17 @@
 with Ada.Text_IO; use Ada.Text_IO;
+
 procedure Main is
+
+  type Instance (Immutable : Positive) is record
+    Mutable : Integer;
+  end record;
+
+  A : Instance := (Immutable => 1, Mutable => 2);
+
 begin
    loop
+      Put(A.Immutable'Image);
+      A.Mutable := 4;
       Put_Line("What is your name?");
       declare
          Name : String := Get_Line;
