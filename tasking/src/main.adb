c8a1f2a0212817f1a7b1a1a171f6fe2827169325
Tama McGlinn <TGJ.McGlinn@mindef.nl>
2018-09-24 10:21:18 +0200

    Added 3 task program

--- tasking/src/main.adb
+++ tasking/src/main.adb
@@ -1,6 +1,20 @@
-with Ada.Strings.Unbounded;  use Ada.Strings.Unbounded;
+with Ada.Text_IO; use Ada.Text_IO;
 
 procedure Main is
+  task First;
+  task Second;
+
+  task body First is
+  begin
+    delay 0.5;
+    Put_Line("First");
+  end First;
+
+  task body Second is
+  begin
+    delay 1.5;
+    Put_Line("Second");
+  end Second;
 begin
-  null;
+  Put_Line("Main");
 end Main;
