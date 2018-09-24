7c49473354b480d6d5801bb20b8316e6d4dc7fbd
Tama McGlinn <TGJ.McGlinn@mindef.nl>
2018-09-24 10:42:41 +0200

    slightly less trivial example that blocks unless insert and remove are alternated by the callers

--- tasking/src/main.adb
+++ tasking/src/main.adb
@@ -1,20 +1,36 @@
 with Ada.Text_IO; use Ada.Text_IO;
 
 procedure Main is
-  task First;
-  task Second;
+  task type Buffer_Task is
+    entry Insert (Item : in Integer);
+    entry Remove (Item : out Integer);
+  end Buffer_Task;
 
-  task body First is
-  begin
-    delay 0.5;
-    Put_Line("First");
-  end First;
+  Buffer_Pool : array (1..15) of Buffer_Task;
+  This_Item : Integer;
 
-  task body Second is
+  task body Buffer_Task is
+    Datum : Integer;
   begin
-    delay 1.5;
-    Put_Line("Second");
-  end Second;
+    loop 
+      select
+        accept Insert(Item : in Integer) do
+          Datum := Item;
+        end Insert;
+      or
+        terminate;
+      end select;
+      select
+        accept Remove(Item : out Integer) do
+          Item := Datum;
+        end Remove;
+      or
+        terminate;
+      end select;
+    end loop;
+  end Buffer_Task;
 begin
-  Put_Line("Main");
+  Buffer_Pool(1).Insert(5);
+  Buffer_Pool(1).Remove(This_Item);
+  Put_Line(This_Item'Image);
 end Main;
